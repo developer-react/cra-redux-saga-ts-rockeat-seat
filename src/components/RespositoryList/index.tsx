62553ed55d86af224b60633675a42c35c80cdd24
Unknown <wagner_gcastro@hotmail.com>
2021-05-04 10:10:43 -0300

    #03 New component ListItem

--- src/components/RespositoryList/index.tsx
+++ src/components/RespositoryList/index.tsx
@@ -6,6 +6,7 @@ import { Repository } from '../../store/ducks/repositories/types';
 import { ApplicationState } from '../../store';
 
 import * as RepositoriesActions from '../../store/ducks/repositories/actions';
+import RepositoryItem from '../RepositoryItem'
 
 interface StateProps {
   repositories: Repository[]
@@ -28,7 +29,9 @@ class RepositoryList extends Component<Props> {
 
     return (
       <ul>
-       { repositories.map(repository => <li>{ repository.name }</li>) }
+        { repositories.map(repository => (
+          <RepositoryItem key={repository.id} repository={repository}/>
+        ))}
       </ul>
     );
   }
